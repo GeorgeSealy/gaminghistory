c963d34c263caa0761cf8c30b329137292a3657b
George Sealy <george@mixbit.com>
2018-07-02 10:43:01 +1200

    Ep 04 - reverse path

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+	const data = {name: 'George', age: 99, here: true};
+ 	// res.send('Hey! It works!');
+ 	// res.json(data);
+ 	// res.send(req.query.name);
+ 	res.json(req.query);
 });
+
+router.get('/reverse/:name', (req, res) => {
+	const reverse = [...req.params.name].reverse().join('');
+	res.send(reverse);
+})
 
 module.exports = router;

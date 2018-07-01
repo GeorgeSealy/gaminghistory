a06cdd5ffa5105fc519ea28f817ba9db3266f814
George Sealy <george@mixbit.com>
2018-07-02 11:34:08 +1200

    Ep 08 - start

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-	const data = {name: 'George', age: 99, here: true};
- 	// res.send('Hey! It works!');
- 	// res.json(data);
- 	// res.send(req.query.name);
- 	// res.json(req.query);
- 	res.render('hello', {
- 		title: 'test',
- 		name: 'George',
- 		dog: req.query.dog
-
- 	});
-});
-
-router.get('/reverse/:name', (req, res) => {
-	const reverse = [...req.params.name].reverse().join('');
-	res.send(reverse);
-})
+router.get('/', storeController.homePage);
 
 module.exports = router;

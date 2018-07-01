f161a1b6b04b74c73076838899297a76cbfc9883
George Sealy <george@mixbit.com>
2018-07-02 11:49:12 +1200

    Ep 09 - start

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,14 @@
+exports.myMiddleware = (req, res, next) => {
+	req.name = 'George';
+
+	// if (req.name === 'George') {
+	// 	throw Error('Not a good path');
+	// }
+	
+	next();
+};
+
 exports.homePage = (req, res) => {
+	console.log(req.name);
  	res.render('index');
  };

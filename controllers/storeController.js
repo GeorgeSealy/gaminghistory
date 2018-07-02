02e60f5a49d5c55333ee5d5cd10d40b787c5e6f6
George Sealy <george@mixbit.com>
2018-07-03 11:58:06 +1200

    Ep 22 - start

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -105,3 +105,10 @@ exports.editStore = async (req, res) => {
  	req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="stores/${store.slug}">View Store →</a>`)
  	res.redirect(`/stores/${store._id}/edit`);
  };
+
+exports.getStoresByTag = async (req, res) => {
+	const tags = await Store.getTagsList();
+	const tag = req.params.tag;
+
+	res.render('tag', { title: 'Tags', tags, tag });
+};
--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,9 @@ router.post('/add/:id',
 	storeController.upload,
 	catchErrors(storeController.resize),
 	catchErrors(storeController.updateStore));
-router.get('/stores/:id/edit', catchErrors(storeController.editStore))
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 module.exports = router;

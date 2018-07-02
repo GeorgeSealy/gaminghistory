9a0ed92870f6d4e509a9ca3d27c8c068a58dc485
George Sealy <george@mixbit.com>
2018-07-02 14:49:08 +1200

    Ep 14 start

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -22,3 +22,10 @@ exports.homePage = (req, res) => {
  	req.flash('success', `Successfully created ${store.name}. Care to leave a review?`)
  	res.redirect(`/store/${store.slug}`);
  };
+
+exports.getStores = async (req, res) => {
+
+	// Query the database for a list of all stores
+	const stores = await Store.find();
+	res.render('stores', { title: 'Stores', stores })
+}

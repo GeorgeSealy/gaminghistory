b612fca1d899823e528a68a72855b8e7f93fd0a1
George Sealy <george@mixbit.com>
2018-07-03 13:24:53 +1200

    Ep 24 - start

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 
 const { catchErrors } = require('../handlers/errorHandlers')
 
@@ -21,5 +22,14 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// Validate registration data
+// register them
+// log them in
+router.post('/register', 
+	userController.validateRegister);
 
 module.exports = router;
